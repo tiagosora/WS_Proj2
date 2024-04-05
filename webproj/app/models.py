6a26c7c9652f52c43bd2afebec212fcc6598ee00
Pjnp5 <paulojnpinto02@ua.pt>
2024-04-06 00:17:21 +0100

    Added models

--- webproj/app/models.py
+++ webproj/app/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
+
 # Create your models here.
+class Wizard(models.Model):
+    name = models.CharField(max_length=255)
+    age = models.IntegerField()
+    house = models.ForeignKey('House', on_delete=models.SET_NULL, null=True)
+    # Assuming there are references to spells and skills in the RDF
+    spells = models.ManyToManyField('Spell')
+    skills = models.ManyToManyField('Skill')
+
+    def __str__(self):
+        return self.name
+
+
+class Spell(models.Model):
+    name = models.CharField(max_length=255)
+    difficulty = models.CharField(max_length=100)
+    effect = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class House(models.Model):
+    name = models.CharField(max_length=255)
+    founder = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Professor(models.Model):
+    name = models.CharField(max_length=255)
+    subject = models.CharField(max_length=255)
+    # Link to the house they belong to or oversee
+    house = models.ForeignKey('House', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=255)
+    level = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
