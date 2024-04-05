064e9b39f59e0fdc9a4b7eb846559996db3d35b7
Tiago Sora <tiagogcarvalho@ua.pt>
2024-04-05 18:43:36 +0100

    app configured

---
+++ webproj/webproj/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    
+    path('', include('app.urls')),
+    
+    path("__reload__/", include("django_browser_reload.urls")),
+]
