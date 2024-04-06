3af59f63eaea4b7a84972a4239abb00cc65ce4c3
Tiago Sora <tiagogcarvalho@ua.pt>
2024-04-06 18:11:46 +0100

    login requirements included and login page created

--- webproj/app/urls.py
+++ webproj/app/urls.py
@@ -1,8 +1,12 @@
-
 from django.urls import path
+from django.contrib.auth.views import LoginView
 
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='')
+    # Testing index
+    path('', views.index, name='index'),
+    
+    # Login
+    path('login/', LoginView.as_view(template_name='app/login.html'),name='login'),
 ]
--- webproj/app/views.py
+++ webproj/app/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
-
-def home(request):
-    
+@login_required
+def index(request):
     return render(request, 'app/index.html')
