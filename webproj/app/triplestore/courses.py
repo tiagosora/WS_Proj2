5a35fe251bee8406692715b7c6bde69fb171f226
Pjnp5 <paulojnpinto02@ua.pt>
2024-04-16 00:05:36 +0100

    updates

--- webproj/app/triplestore/courses.py
+++ webproj/app/triplestore/courses.py
@@ -8,41 +8,38 @@ from app.triplestore.utils import execute_sparql_query
 
 def get_courses_uri_by_professor_uri(professor_uri):
     query_name = "app/queries/get_courses_uri_by_professor_uri.sparql"
-    
+
     results, _ = execute_sparql_query(query_name, format='JSON', user_uri=professor_uri)
-    
+
     total_courses = []
     for elem in results["results"]["bindings"]:
         total_courses.append(elem["courses"]["value"])
-        
+
     return total_courses
 
-def update_learned_to_is_learning(course_id, student_id):   #teoricamente, useless
-    query_name = "app/queries/update_learned_to_is_learning.sparql"
-    execute_sparql_query(query_name=query_name, format='POST', course_id=course_id, student_id=student_id)
-    
+
 def get_courses_dict():
-    
     query_name = "app/queries/get_all_by_type.sparql"
     results, _ = execute_sparql_query(query_name=query_name, format='JSON', type="course")
-    
+
     courses = []
     for elem in results["results"]["bindings"]:
         courses.append(get_course_info(elem["data"]["value"]))
-        
+
     courses_dict = {course.id: course.info_no_id()
-                | {'spells': manage_spells_list(course.teaches_spell)}
-                # | {'is_learning': get_students_enrolled(course.id)}
-                # | {'learned': get_students_finished_course(course.id)}
-                | {'professor_info': get_wizard_info_by_uri(get_professor(course.professor).wizard).info()}
-                for course in courses}
-    
+                               | {'spells': manage_spells_list(course.teaches_spell)}
+                               # | {'is_learning': get_students_enrolled(course.id)}
+                               # | {'learned': get_students_finished_course(course.id)}
+                               | {'professor_info': get_wizard_info_by_uri(
+        get_professor(course.professor).wizard).info()}
+                    for course in courses}
+
     return courses_dict
 
+
 def get_course_by_id_dict(course_id):
-    
     course = get_course_info(course_uri=f"http://hogwarts.edu/courses/{course_id}")
-    
+
     course_info = course.info()
     spells = manage_spells_list(course.teaches_spell)
     is_learning = get_students_enrolled(course.id)
@@ -57,37 +54,43 @@ def get_course_by_id_dict(course_id):
         'professor_info': professor_info
     }
 
-    
+
 def update_is_learning_to_learned(course_id, student_id):
     query_name = "app/queries/update_is_learning_to_learned.sparql"
     execute_sparql_query(query_name=query_name, format='POST', course_id=course_id, student_id=student_id)
-    
+
+
 def add_spell_to_course(course_id, spell_id):
     query_name = "app/queries/add_spell_to_course.sparql"
     execute_sparql_query(query_name=query_name, format='POST', course_id=course_id, spell_id=spell_id)
-    
+
+
 def remove_spell_from_course(course_id, spell_id):
     query_name = "app/queries/remove_spell_from_course.sparql"
     execute_sparql_query(query_name=query_name, format='POST', course_id=course_id, spell_id=spell_id)
-    
+
+
 def add_student_to_course(course_id, student_id):
     query_name = "app/queries/add_student_to_course.sparql"
     execute_sparql_query(query_name=query_name, format='POST', course_id=course_id, student_id=student_id)
 
-def remove_student_from_course(course_id, student_id):  #teoricamente, useless
+
+def remove_student_from_course(course_id, student_id):  # teoricamente, useless
     query_name = "app/queries/remove_student_from_course.sparql"
     execute_sparql_query(query_name=query_name, format='POST', course_id=course_id, student_id=student_id)
-    
+
+
 def change_course_professor(course_id, professor_id):
     query_name = "app/queries/change_course_professor.sparql"
     execute_sparql_query(query_name=query_name, format='POST', course_id=course_id, professor_id=professor_id)
 
+
 def get_len_all_courses():
     query_name = "app/queries/get_len_all_courses.sparql"
     results, _ = execute_sparql_query(query_name=query_name, format='JSON')
-    
+
     len_courses = None
-    if (len(results["results"]["bindings"])>0):
+    if (len(results["results"]["bindings"]) > 0):
         len_courses = int(results["results"]["bindings"][0]["count"]["value"])
-        
+
     return len_courses
